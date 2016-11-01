15453ea4594b657facb6564cb82f20937135bbcd
zzzborowski <zzzbor@gmail.com>
2016-11-01 11:14:56 +0100

    Add files via upload

---
+++ Documents/RWorkspace/data_field_codes_with_multi_data_fields.py
@@ -0,0 +1,32 @@
+
+
+import pandas as pd
+
+import numpy as np
+
+
+
+zika = pd.read_csv('zika_cleared.csv', low_memory=False)
+
+
+
+
+
+grouped_by_code = zika.groupby('data_field_code')
+
+
+
+data_field_codes = grouped_by_code.groups.keys()
+
+
+
+
+#Returns all data_field_code records with more than 1 data_field entry
+
+for key in data_field_codes:
+
+    if len(grouped_by_code.get_group(key).data_field.unique()) > 1:
+
+        print(key, grouped_by_code.get_group(key).data_field.unique())
+
+
